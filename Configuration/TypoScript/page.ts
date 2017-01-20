30caad8cc60557e172f6f127c9ddba1843a9001a
Joe Schäfer <schaefer@scw.info>
2017-01-20 15:18:11 +0100

    RTE und Textfarben

--- Configuration/TypoScript/page.ts
+++ Configuration/TypoScript/page.ts
@@ -18,23 +18,41 @@ RTE.default {
   #angepasste CSS Datei in fileadmin speichern
   contentCSS = EXT:fetschersite/Resources/Public/Css/rte.css
 }
-## Klassen hinzufuegen 
-RTE.default.proc.allowedClasses := addToList(button-link) 
+## Klassen hinzufuegen
+RTE.default.proc.allowedClasses := addToList(button-link)
 RTE.default.buttons {
-     # blockstyle.tags.div.allowedClasses := addToList(error, small) 
-     # textstyle.tags.span.allowedClasses := addToList(small) 
-     link.properties.class.allowedClasses := addToList(button-link) 
-} 
-## nicht benoetigte Klassen entfernen 
-RTE.default.proc.allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2, important, name-of-person, detail) 
+     # blockstyle.tags.div.allowedClasses := addToList(error, small)
+     # textstyle.tags.span.allowedClasses := addToList(small)
+     link.properties.class.allowedClasses := addToList(button-link)
+}
+## nicht benoetigte Klassen entfernen
+RTE.default.proc.allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2, important, name-of-person, detail)
 RTE.default.buttons {
   blockstyle.tags.div.allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2)
-} 
-RTE.classes := removeFromList(csc-frame-frame1, csc-frame-frame2, important, name-of-person, detail) 
+}
+RTE.classes := removeFromList(csc-frame-frame1, csc-frame-frame2, important, name-of-person, detail)
 ## Klassen fuer die RTE Auswahlfelder definieren
 
-RTE.classesAnchor.button-link { 
+RTE.classesAnchor.button-link {
       name = Linkbutton
       #value = background: url(fileadmin/../arrow1.gif) no-repeat 100% 0%;
       class= btn btn-warning
  }
+
+##Button Textfarbe einblenden
+RTE.default.showButtons := addToList (textcolor)
+
+##Farbpicker ausblenden
+#RTE.default.disableColorPicker = 1
+
+RTE.default.colors = color1, color2, color3
+RTE.colors {
+   color1 {
+      name = Inhalte fehlen noch
+      value = #FF69B4
+   }
+   color2 {
+      name = Inhalte überprüfen
+      value = #FF7D00
+   }
+}

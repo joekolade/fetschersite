00086cc9f0420b232ad6bd3bb4eede1877a10989
Joe Schäfer <schaefer@scw.info>
2017-02-24 16:39:23 +0100

    Powermail offset classes

--- Configuration/TypoScript/Constants/tx_powermail.ts
+++ Configuration/TypoScript/Constants/tx_powermail.ts
@@ -3,3 +3,6 @@ plugin.tx_powermail.settings.main.confirmation = 0
 plugin.tx_powermail.settings.main.optin = 0
 plugin.tx_powermail.settings.main.moresteps = 0
 plugin.tx_powermail.settings.misc.ajaxSubmit = 1
+
+plugin.tx_powermail.settings.styles.bootstrap.offsetClasses =
+plugin.tx_powermail.settings.styles.bootstrap.offsetClassesGeneral = col-sm-offset-2
---
+++ Configuration/TypoScript/Setup/tx_powermail.ts
@@ -0,0 +1,1 @@
+plugin.tx_powermail.settings.setup.styles.framework.offsetClasses = {$plugin.tx_powermail.settings.styles.bootstrap.offsetClassesGeneral}
